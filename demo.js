fae45b9a2fa3068b590e56f6bd1ff68d06bb9dcc
Joshua Vial <joshua@enspiral.com>
2021-02-16 23:27:51 +1300

    setting up kraken api

---
+++ demo.js
@@ -0,0 +1,25 @@
+require('dotenv').config()
+const KrakenClient = require('kraken-api')
+
+const key = process.env.KRAKEN_API_KEY
+const secret = process.env.KRAKEN_API_SECRET
+const kraken = new KrakenClient(key, secret)
+
+// https://www.kraken.com/features/api
+
+// Balance
+// Assets
+// AssetPairs
+// Ticker
+// OpenOrders
+// CancelOrder
+// AddOrder
+
+kraken.api('OpenOrders')
+  .then(res => {
+    console.log(res.result)
+    return null
+  })
+  .catch(e => {
+    console.log(e)
+  })
