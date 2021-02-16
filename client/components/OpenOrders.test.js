ee89735e8ad0f788e3884745e60696902ca1b80d
Warahi Spenceley <wizzzo@protonmail.com>
2021-02-17 12:04:29 +1300

    linting

--- client/components/OpenOrders.test.js
+++ client/components/OpenOrders.test.js
@@ -6,5 +6,5 @@ import OpenOrders from './OpenOrders'
 test('render a <tr></tr> for each order item', () => {
   render(<OpenOrders/>)
   const tr = (screen.getAllByRole('row'))
-  expect(tr.length).toEqual(4)
+  expect(tr).toHaveLength(4)
 })
