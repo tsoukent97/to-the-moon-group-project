46386288a7612d2a8e5c5b30870f71e7f9b3dab2
Warahi Spenceley <wizzzo@protonmail.com>
2021-02-17 12:03:06 +1300

    test correct number of table rows display

---
+++ client/components/OpenOrders.test.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import { screen, render } from '@testing-library/react'
+
+import OpenOrders from './OpenOrders'
+
+test('render a <tr></tr> for each order item', () => {
+  render(<OpenOrders/>)
+  const tr = (screen.getAllByRole('row'))
+  expect(tr.length).toEqual(4)
+})
