e00ff4f08fa4fb1932dc49ebe5fb6663daf2962f
Benjamin Trainor <benjamin.trainor.js@protonmail.com>
2021-02-17 17:53:06 +1030

    un commited change.. forgot to save file prior to earlier push

--- client/components/Balance.test.js
+++ client/components/Balance.test.js
@@ -9,28 +9,26 @@ test('render a <tr></tr> for each balance item', () => {
   expect(tr).toHaveLength(1)
 })
 
-jest.mock('./index', () => {
-  return { getBalance: jest.fn() }
-})
+// jest.mock('./index', () => {
+//   return { getBalance: jest.fn() }
+// })
 
-describe('getBalance', () => {
-  test('is working', () => {
-    expect.assertions(1)
-    expect(1).toEqual(1)
-  })
+// describe('getBalance', () => {
+//   test('is working', () => {
+//     expect.assertions(1)
+//     expect(1).toEqual(1)
+//   })
 
-  test('receiving data from server side API', () => {
-    expect.assertions(1)
-    getBalance.mockImplementation(() => {
-      return Promise.resolve([
-        { token: 'BTC', amount: '0.001', usdPrice: '49500.00', usdValue: '495.00' },
-        { token: 'USD', amount: '10', usdPrice: '1', usdValue: '10' }
-      ])
-    })
-    request(server).get('./balance'
-    )
-  })
-})
+//   test('receiving data from server side API', () => {
+//     expect.assertions(1)
+//     getBalance.mockImplementation(() => {
+//       return Promise.resolve([
+//         { token: 'BTC', amount: '0.001', usdPrice: '49500.00', usdValue: '495.00' },
+//         { token: 'USD', amount: '10', usdPrice: '1', usdValue: '10' }
+//       ])
+//     })
+//   })
+// })
 
 // test('', () => {
 //   render(<Balance />)
