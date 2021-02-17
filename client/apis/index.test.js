4cc51bc67c98c6fe97c1bf79fc158a9c5be360ea
Joshua Vial <joshua@enspiral.com>
2021-02-18 11:25:38 +1300

    jv comments

--- client/apis/index.test.js
+++ client/apis/index.test.js
@@ -1,12 +1,16 @@
 import request from 'supertest'
 import express from 'express'
-import getBalance from './index'
 
+import { getBalance } from './index'
+
+//remove the mock for getBalance
 jest.mock('./index', () => {
   return getBalance: jest.fn()
 })
 
+
 describe('getBalance', () => {
+  //get rid of this as soon as it works
   test('is working', () => {
     expect.assertions(1)
     expect(1).toEqual(1)
@@ -14,12 +18,19 @@ describe('getBalance', () => {
 
   test('receiving data from server side API', () => {
     expect.assertions(1)
+    //create a nock for '/apiv1/balance'
+    //have the nock send back the fakeData
+
+    fakeData = ['data that looks real']
+    fakeData = {key: 'whatever i want'}
     getBalance.mockImplementation(() => {
       return Promise.resolve([
           { token: 'BTC', amount: '0.001', usdPrice: '49500.00', usdValue: '495.00' },
           { token: 'USD', amount: '10', usdPrice: '1', usdValue: '10' }
       ])
     })
+    //execute getBalance
+    //assert it sends back fakeData
     request(server).get('./balance'
     )
 
--- client/components/Balance.test.js
+++ client/components/Balance.test.js
@@ -7,21 +7,27 @@ import Balance from './Balance'
 jest.mock('../apis')
 
 test('render a <tr></tr> for each balance item', () => {
+  //mock out getBalance
   render(<Balance />)
   const tr = (screen.getAllByRole('row'))
   expect(tr).toHaveLength(1)
 })
 
-test('sadfuydiflu', async () => {
+test.only('sadfuydiflu', async () => {
   getBalance.mockImplementation(() => Promise.resolve([
     { token: 'BTC', amount: '0.001', usdPrice: '49500.00', usdValue: '495.00' },
     { token: 'USD', amount: '10', usdPrice: '1', usdValue: '10' }]))
   render(<Balance />)
+
+  await waitFor(() => getBalance.mock.calls.length > 0)
+
+  //try using getAllByRole('gridcell')
   const cryptoToken = screen.getAllByTestId('testData')
+  //screen.debug()
   expect(cryptoToken.innerHTML).toEqual(4)
   const heading = screen.getByRole('heading')
   expect(heading.innerHTML).toEqual('Wallet Balance')
-  await waitFor(() => getBalance.mock.calls.length > 0)
+
 })
 
 // jest.mock('./index', () => {
