904d355bb4bc259dfe71e8764dd7fe6b44fa4c0d
Justin Cole <justincole@outlook.co.nz>
2021-02-17 21:26:18 +1300

    refactoring

--- server/kraken.js
+++ server/kraken.js
@@ -6,26 +6,27 @@ const secret = process.env.KRAKEN_API_SECRET
 const kraken = new KrakenClient(key, secret)
 
 function getBalances() {
-  return kraken.api('Balance')
-    .then(balance => {
-      const tokens = Object.keys(balance.result).slice(1)
-      const pairs = tokens.map(token => token + 'ZUSD').join(', ')
-      return {pairs, tokens, balance}
-    })
+  return kraken
+    .api('Balance')
+    .then(balance => balance)
     .catch(err => console.log(err))
 }
 
-function getAssetInfo({pairs, tokens, balance}) {
-  return kraken.api('Ticker', { pair: pairs }).then(response => {
-    return tokens.map((asset) => {
+function getAssetInfo(balance) {
+  const tokens = Object.keys(balance.result).slice(1)
+  const pairs = tokens.map(token => token + 'ZUSD').join(', ')
+  return kraken.api('Ticker', { pair: pairs }).then(response =>
+    tokens.map(token => {
+      const amount = balance.result[token]
+      const priceUsd = response.result[token + 'ZUSD'].c[0]
       return {
-          token: asset,
-          amount: balance.result[asset],
-          priceUsd: response.result[asset + 'ZUSD'].c[0],
-          amountUsd: balance.result[asset] * response.result[asset + 'ZUSD'].c[0]
-        }
+        token,
+        amount,
+        priceUsd,
+        amountUsd: amount * priceUsd,
+      }
     })
-  })
+  )
 }
 
 module.exports = { getBalances, getAssetInfo }
