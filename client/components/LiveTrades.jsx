9037cd1d3eda4fdaac62a855d11bd96aa48e8ab2
Lache Melvin <lache.melvin@gmail.com>
2021-02-18 11:43:50 +1300

    display live trades

--- client/components/LiveTrades.jsx
+++ client/components/LiveTrades.jsx
@@ -1,23 +1,69 @@
-import React, { useEffect } from 'react'
+import React, { useState, useEffect } from 'react'
 
 function LiveTrades () {
   const socket = new WebSocket('wss://ws.kraken.com')
+  const [trades, setTrades] = useState([])
 
   useEffect(() => {
     socket.addEventListener('open', () => {
-      socket.send(JSON.stringify({ event: 'ping', reqid: 123 }))
+      socket.send(JSON.stringify({
+        event: 'subscribe',
+        pair: [
+          'XBT/USD'
+        ],
+        subscription: {
+          name: 'trade'
+        }
+      }))
     })
-    socket.addEventListener('message', (data) => {
-      console.log(data)
+
+    socket.addEventListener('close', () => {
+      socket.send(JSON.stringify({
+        event: 'unsubscribe',
+        pair: [
+          'XBT/USD'
+        ],
+        subscription: {
+          name: 'trade'
+        }
+      }))
     })
+
     return () => {
       socket.close()
     }
   }, [])
+
+  socket.addEventListener('message', (res) => {
+    const result = JSON.parse(res.data)
+    result.length && addNewTrades(result[1])
+  })
+
+  function addNewTrades (newTrades) {
+    setTrades((prevTrades) => [...prevTrades, ...newTrades])
+  }
+
   return (
-    <>
-     hello
-    </>
+    <table>
+      <thead>
+        <tr>
+          <th>Price</th>
+          <th>Volume</th>
+          <th>Side</th>
+          <th>Type</th>
+        </tr>
+      </thead>
+      <tbody>
+        {trades.map((trade) => {
+          return <tr key={trade[2]}>
+            <td>{trade[0]}</td>
+            <td>{trade[1]}</td>
+            <td>{trade[3] === 'b' ? 'Buy' : 'Sell'}</td>
+            <td>{trade[4] === 'l' ? 'Limit' : 'Market'}</td>
+          </tr>
+        })}
+      </tbody>
+    </table>
   )
 }
 
