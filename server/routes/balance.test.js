2b02a4c7e9a094ffb5496d2eb556e9606b47d74c
Justin Cole <justincole@outlook.co.nz>
2021-02-18 13:13:40 +1300

    kraken function tests

--- server/routes/balance.test.js
+++ server/routes/balance.test.js
@@ -1,54 +1,61 @@
 const request = require('supertest')
 
 const server = require('../server.js')
-const { getBalances, getAssetInfo } = require('../kraken')
+const { getBalances, getAssetInfo, mapAssetInfo } = require('../kraken')
 
 jest.mock('../kraken', () => ({
   getBalances: jest.fn(),
   getAssetInfo: jest.fn()
 }))
 
-test('getBalances resolves with balance object', () => {
-  getBalances.mockImplementation(() => {
-    return Promise.resolve({
-      balance: {
-        error: [],
-        result: {
-          ZUSD: '9.9840',
-          XXBT: '0.0017530100',
-          XLTC: '0.0001500000',
-          XETH: '0.0000068000'
-        }
+describe('kraken.js functions', () => {
+  const balanceResponse = {
+    balance: {
+      error: [],
+      result: {
+        ZUSD: '9.9840',
+        XXBT: '0.0017530100',
+        XLTC: '0.0001500000',
+        XETH: '0.0000068000'
       }
+    }
+  }
+  const assetInfoResponse = [
+    {
+      token: 'LUKLHD',
+      amount: 48754,
+      priceUsd: 34598,
+      amountUsd: 423024
+    }
+  ]
+
+  test('calls getBalances', () => {
+    getBalances.mockImplementation(() => {
+      return Promise.resolve(balanceResponse)
+    })
+    return getBalances().then(actual => {
+      expect(actual).toEqual(balanceResponse)
+      return null
     })
   })
-  getAssetInfo.mockImplementation(() => {
-    return Promise.resolve([
-      {
-        token: 'LUKLHD',
-        amount: 48754,
-        priceUsd: 34598,
-        amountUsd: 423024
-      }
-    ])
-  })
-  request(server)
-    .get('/api/v1/balance')
-    .expect('Content-Type', /json/)
-    .expect(200)
-    .then(res => {
-      expect(res).toBe([
-        {
-          token: 'LUKLHD',
-          amount: 48754,
-          priceUsd: 34598,
-          amountUsd: 423024
-        }
-      ])
-      // more tests about the response
-      expect(getBalances).toHaveBeenCalled()
-      expect(getAssetInfo).toHaveBeenCalled()
-      return res
+
+  test('calls getAssetInfo', () => {
+    getAssetInfo.mockImplementation(() => {
+      return Promise.resolve(assetInfoResponse)
     })
-    .catch(err => err.message)
+    return getAssetInfo().then(actual => {
+      expect(actual).toEqual(assetInfoResponse)
+      return null
+    })
+  })
+
+  test('calls mapAssetInfo and return the right data', () => {
+    const token = 'XXBT'
+    const balance = balanceResponse
+    const assetInfo = { result: { XXBTZUSD: { c: [34598] } } }
+
+    const actual = mapAssetInfo(token, assetInfo, balance)
+
+    expect(actual).toEqual(assetInfoResponse[0])
+  })
 })
