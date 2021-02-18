fb2be501e292baa1cdfa1fa7d878c407c40a5b82
Warahi Spenceley <wizzzo@protonmail.com>
2021-02-18 15:30:23 +1300

    fix orders.test.js

--- server/server.js
+++ server/server.js
@@ -1,7 +1,6 @@
 const express = require('express')
 const path = require('path')
 
-const fruitRoutes = require('./routes/fruits')
 const orderRoutes = require('./routes/orders')
 
 const server = express()
@@ -9,7 +8,6 @@ const server = express()
 server.use(express.json())
 server.use(express.static(path.join(__dirname, 'public')))
 
-server.use('/api/v1/fruits', fruitRoutes)
 server.use('/api/v1/orders', orderRoutes)
 
 module.exports = server
