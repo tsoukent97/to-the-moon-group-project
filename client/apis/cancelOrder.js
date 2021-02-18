3d0a1fbf3bb6d9b65ff7db81f5af98cca5569ded
jordan-tuinman <jojotuijo@gmail.com>
2021-02-18 16:23:36 +1300

    merge with main - includes come basic files for new feature

---
+++ client/apis/cancelOrder.js
@@ -0,0 +1,11 @@
+import request from 'superagent'
+
+const rootUrl = '/api/v1'
+
+// TODO: create post request for connected post route (Warahi & Benjamin ticket)
+
+// export function cancelOrder () {
+//   return request.post(rootUrl + /orders/cancel/:id)
+//     .then()
+//     .catch(e => console.log(e))
+// }
