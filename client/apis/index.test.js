f0f03a2eec251fb54a052a16caef17c4c7c88748
Benjamin Trainor <benjamin.trainor.js@protonmail.com>
2021-02-18 12:24:16 +1030

    client api test

--- client/apis/index.test.js
+++ client/apis/index.test.js
@@ -1,38 +1,30 @@
-import request from 'supertest'
-import express from 'express'
+import nock from 'nock'
 
 import { getBalance } from './index'
 
-//remove the mock for getBalance
-jest.mock('./index', () => {
-  return getBalance: jest.fn()
-})
-
-
 describe('getBalance', () => {
-  //get rid of this as soon as it works
-  test('is working', () => {
-    expect.assertions(1)
-    expect(1).toEqual(1)
-  })
+  // fakeData = ['data that looks real']
+  // fakeData = { key: 'whatever i want' }
+  const testData = [
+    { token: 'BTC', amount: '0.001', usdPrice: '49500.00', usdValue: '495.00' },
+    { token: 'USD', amount: '10', usdPrice: '1', usdValue: '10' }
+  ]
+
+  // create a nock for '/apiv1/balance'
+  const scope = nock('http://localhost')
+    .get('/api/v1/balance/')
+    // have the nock send back the fakeData
+    .reply(200, testData)
 
   test('receiving data from server side API', () => {
-    expect.assertions(1)
-    //create a nock for '/apiv1/balance'
-    //have the nock send back the fakeData
-
-    fakeData = ['data that looks real']
-    fakeData = {key: 'whatever i want'}
-    getBalance.mockImplementation(() => {
-      return Promise.resolve([
-          { token: 'BTC', amount: '0.001', usdPrice: '49500.00', usdValue: '495.00' },
-          { token: 'USD', amount: '10', usdPrice: '1', usdValue: '10' }
-      ])
-    })
-    //execute getBalance
-    //assert it sends back fakeData
-    request(server).get('./balance'
-    )
-
+    expect.assertions(2)
+    // execute getBalance
+    // assert it sends back fakeData
+    return getBalance()
+      .then(newData => {
+        expect(newData).toEqual(testData)
+        expect(scope.isDone()).toBe(true)
+        return null
+      })
   })
 })
