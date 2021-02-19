7feed1014f7b9446a860289df1cfb8d6a8277b27
Justin Cole <justincole@outlook.co.nz>
2021-02-19 14:12:36 +1300

    begun addorder component

---
+++ client/components/AddOrder.jsx
@@ -0,0 +1,10 @@
+import React, { useState } from 'react'
+
+const AddOrder = () => {
+  const [order, setOrder] = useState({})
+  const [text, setText] = useState('')
+
+  return <div></div>
+}
+
+export default AddOrder
