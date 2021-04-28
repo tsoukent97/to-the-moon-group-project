8126013a37a27983a4d0a62915a0110988803f36
cherrycrush <phobosnz@gmail.com>
2021-04-28 16:00:04 +1200

    Users database ready for authenticare team

--- server/db/migrations/20210428154131_users.js
+++ server/db/migrations/20210428154131_users.js
@@ -1,11 +1,11 @@
-exports.up = function(knex, Promise) {
-  return knex.schema.createTable(('users', table => {
+exports.up = (knex, Promise) => {
+  return knex.schema.createTable('users', table => {
     table.increments('id')
     table.string('username')
     table.binary('hash')
   })
 }
 
-exports.down = function(knex, Promise) {
+exports.down = (knex, Promise) => {
   return knex.schema.dropTable('users')
 }
