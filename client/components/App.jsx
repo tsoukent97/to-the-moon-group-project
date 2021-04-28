0f34c0cddb96488f29eeefab69b6fc0b0aa17a54
nicole-ilustre <nicoleilustre2292@gmail.com>
2021-04-28 16:30:49 +1200

    add chart component

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -4,11 +4,13 @@ import Balance from './Balance.jsx'
 import OpenOrders from './OpenOrders'
 import AddOrder from './AddOrder'
 import LiveTrades from './LiveTrades'
+import CandlestickChart from './CandlestickChart'
 
 function App () {
   return (
     <>
       <div className="app">
+        <CandlestickChart />
         <Balance />
         <OpenOrders />
         <AddOrder />
