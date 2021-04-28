fbd3c2cd4fa357fecd2beec5c9ff848787a1b3c5
cherrycrush <phobosnz@gmail.com>
2021-04-28 15:45:37 +1200

    Create user migrations

---
+++ server/db/migrations/20210428154131_users.js
@@ -0,0 +1,11 @@
+exports.up = function(knex, Promise) {
+  return knex.schema.createTable(('users', table => {
+    table.increments('id')
+    table.string('username')
+    table.binary('hash')
+  })
+}
+
+exports.down = function(knex, Promise) {
+  return knex.schema.dropTable('users')
+}
