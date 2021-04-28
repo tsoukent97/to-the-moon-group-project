291493ba73e3bb16beaec000c65d9c99ebc0e7cf
cherrycrush <phobosnz@gmail.com>
2021-04-28 16:34:50 +1200

    Fix linting errors

--- server/db/seeds/test-log.js
+++ server/db/seeds/test-log.js
@@ -5,9 +5,9 @@ exports.seed = function(knex) {
     .then(function () {
       // Inserts seed entries
       return knex('audit-log').insert([
-        { id: 1, order_type: 'Add', created_at: '25 December 2012'},
-        { id: 2, order_type: 'Cancel', created_at: '26 December 2012'},
-        { id: 3, order_type: 'Add', created_at: '27 December 2012'}
+        { id: 1, order_type: 'Add', created_at: '25 December 2012' },
+        { id: 2, order_type: 'Cancel', created_at: '26 December 2012' },
+        { id: 3, order_type: 'Add', created_at: '27 December 2012' }
       ]);
     });
 };
