093fac6950c6f30d9174e355f2cab23881f55356
nathan-mclauchlan <nathanmclauchlan21@gmail.com>
2021-04-29 12:11:31 +1200

    removed console.logs, refactored usestate

--- client/apis/index.test.js
+++ client/apis/index.test.js
@@ -1,6 +1,6 @@
 import nock from 'nock'
 
-import { getBalance, cancelOrder } from './index'
+import { getBalance, cancelOrder, getOrders, addOrder } from './index'
 
 describe('getBalance', () => {
   const testData = [
@@ -65,5 +65,89 @@ describe('cancelOrder - failed', () => {
         expect(scope.isDone()).toBe(true)
         return null
       })
+  })
+})
+
+describe('getOrders', () => {
+  const fakeBody = [
+    {
+      id: 'OTGZ4R-5DLAK-2LOQCQ',
+      opentm: 1613470428.006,
+      vol: '0.00020000',
+      price: '52000.0',
+      pair: 'XBTUSD',
+      type: 'sell'
+    },
+    {
+      id: 'O7ZXP3-MXHKG-4NWUHE',
+      opentm: 1613470176.8885,
+      vol: '0.00020000',
+      price: '65000.0',
+      pair: 'XBTUSD',
+      type: 'sell'
+    },
+    {
+      id: 'OZVQHL-OE55C-S3DYN2',
+      opentm: 1613470166.8788,
+      vol: '0.00020000',
+      price: '60000.0',
+      pair: 'XBTUSD',
+      type: 'sell'
+    }
+  ]
+  const scope = nock('http://localhost').get('/api/v1/orders/open').reply(200, fakeBody)
+
+  test('returns body of response', () => {
+    return getOrders()
+      .then((orders) => {
+        expect(orders).toEqual(fakeBody)
+        expect(scope.isDone()).toBe(true)
+        return null
+      })
+  })
+})
+
+describe('addOrder', () => {
+  const testAddOrder =
+  {
+    pair: 'XXBTZUSD',
+    price: '65000.0',
+    type: 'sell'
+  }
+  // afterEach(() => nock.cleanAll())
+
+  describe('when happy', () => {
+    const scopeSuccess = nock('http://localhost')
+      .post('/api/v1/orders/add', { order: testAddOrder })
+      .reply(201)
+
+    test('posts new order', () => {
+      expect.assertions(2)
+
+      return addOrder(testAddOrder)
+        .then((response) => {
+          expect(response).toBeNull()
+          expect(scopeSuccess.isDone()).toBe(true)
+          return null
+        })
+    })
+  })
+
+  describe('api call fails', () => {
+    const scopeFail = nock('http://localhost')
+      .post('/api/v1/orders/add', { order: testAddOrder })
+      .reply(500, 'oh no!')
+
+    test('when call fails', () => {
+      expect.assertions(3)
+
+      return addOrder(testAddOrder)
+        .catch((err) => {
+          expect(err.status).toBe(500)
+          expect(err.response.text).toBe('oh no!')
+          expect(scopeFail.isDone()).toBe(true)
+          return null
+        })
+    })
   })
 })
