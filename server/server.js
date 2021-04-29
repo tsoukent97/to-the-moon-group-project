70e5de5c2134a2000c74265f6174839ab5098b8d
Joshua Vial <joshua@enspiral.com>
2021-04-29 17:39:51 +1200

    removing unused request

--- server/server.js
+++ server/server.js
@@ -1,6 +1,5 @@
 const express = require('express')
 const path = require('path')
-const request = require('superagent')
 
 const balanceRoutes = require('./routes/balance')
 const orderRoutes = require('./routes/orders')
