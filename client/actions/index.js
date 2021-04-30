beb7c136e4195ca35d97034db6ce2e4e43eb23e8
sam-taylor165 <taylsj1908@gmail.com>
2021-04-30 13:23:18 +1200

    start closeError function

--- client/actions/index.js
+++ client/actions/index.js
@@ -1,8 +1,16 @@
 export const ERROR_MESSAGE = 'ERROR_MESSAGE'
+export const CLOSE_MESSAGE = 'CLOSE_MESSAGE'
 
 export function setError (errorMessage) {
   return {
     type: ERROR_MESSAGE,
+    errorMessage: errorMessage
+  }
+}
+
+export function closeErrorMessage (errorMessage) {
+  return {
+    type: CLOSE_MESSAGE,
     errorMessage: errorMessage
   }
 }
--- client/components/DisplayError.jsx
+++ client/components/DisplayError.jsx
@@ -1,14 +1,27 @@
 import React from 'react'
 import { connect } from 'react-redux'
+import { closeErrorMessage } from '../actions'
+import { getBalance } from '../apis/index'
 
-function ErrorMessage (props) {
+const ErrorMessage = (props, { dispatch }) => {
+  const closeError = () => {
+    getBalance()
+      .then(() => {
+        dispatch(closeErrorMessage('')
+        )
+        return null
+      })
+      .catch(e => {
+        console.log('this doesnt work')
+      })
+  }
+
   return (
     <div className='error'>
-      {props.errorMessage}
+      {props.errorMessage === '' ? props.errorMessage : <div>{props.errorMessage}<button onClick={closeError}>X</button></div> }
     </div>
   )
 }
-
 function mapStateToProps (state) {
   return {
     errorMessage: state.errorMessage
